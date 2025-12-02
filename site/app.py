7782520de431548022f90e35caee7d2fd9b20a9c
PteroisRadiata <144462615+PteroisRadiata@users.noreply.github.com>
2025-12-02 13:08:24 -0700

    Integrate Supabase and enhance UI in app.py
    Refactored app.py to integrate Supabase client and update UI components.

--- site/app.py
+++ site/app.py
@@ -1,22 +1,83 @@
 import streamlit as st
+from supabase import create_client, Client
 import pandas as pd
-import psycopg2
+from streamlit_folium import st_folium
+import folium
 
-db_url = st.secrets["connections"]["supabase"]
+st.title("Solar Panel Deployment Simulator")
 
+# Load secrets
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_ANON_KEY"]
 
+supabase: Client = create_client(url, key)
 
-st.title("EIA Hourly Demand from supabase db")
+# Query example table
+#response = supabase.table("TEPC_demand").select("*").limit(3).execute()
+#df = pd.DataFrame(response.data)
+#st.dataframe(df)
 
+df1 = pd.DataFrame({
+    'first column': ['Ann Arbor', 'Tucson'],
+    })
 
-# connection to db 
-conn = psycopg2.connect(db_url)
+option = st.selectbox(
+    'Select city',
+    df1['first column'])
 
-# Simple query
-query = "SELECT * FROM eia_hourly_demand LIMIT 10"
+# Map container
+map_container = st.container(border=True)
+with map_container:
+    st.map(data=df1.rename(columns={'first column': 'city'}).assign(lat=[42.2808, 32.2226], lon=[-83.7430, -110.9747]))
+    #m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    #folium.Marker(
+    #    [39.949610, -75.150282],
+    #    popup="Liberty Bell",
+    #    tooltip="Liberty Bell"
+    #).add_to(m)
 
-df = pd.read_sql(query, conn)
+    # call to render Folium map in Streamlit
+    #st_data = st_folium(m, width = 725)
+    
+# City specs container
+city_specs = '''
+                Number of suitable buildings: {}\n
+                Available area for residential buildings: {}sqft\n
+                Available area for commercial buildings: {}sqft\n
+                Current energy usage: {}kwh\n
+                Current energy cost: ${}\n
+                Average solar radiation: {}kwh/m2/day
+                '''
+with st.container(height=300):
+    st.markdown(city_specs)
 
-st.dataframe(df)
+# Power percentage slider
+st.slider("Select Power Percentage", 0, 100, 0)
+# Commercial coverage slider
+st.slider("Select Commercial Coverage", 0, 100, 0)
 
-conn.close()
+# Results container
+results_container = '''
+                    RESULTS\n
+                    Total number of buildings required to meet demand: {}\n
+                    Residential Buildings: {}\n
+                    Commercial Buildings: {}\n
+
+                    Solar Potential\n
+                    System size: {}kw\n
+                    Annual electricity production: {}kwh\n
+                    Roof size suitability: {}sqft\n
+
+                    Financial Benefits:\n
+                    System cost: ${}\n
+                    First year savings: ${}\n
+                    25 year savings: ${}\n
+                    Payback period: {} years\n
+
+                    Environmental Benefits:\n
+                    Annual CO2 savings: {}lbs CO2e\n
+                    Total greenhouse gas reduction over 25 years: {}lbs CO2e\n
+                    Equivalent trees planted: {}
+                    '''
+with st.container(height=300):
+    st.markdown(results_container)
